1a9c75ff415fa8a6dffec886e2aad7adf236d0bf
Keith Resar <keith.resar@422long.com>
2017-04-24 20:22:31 -0500

    -

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Data sourced from:
+https://sourceforge.net/projects/fplan/files/fplan-db/2015.10.15/
+
+This depends on the Mac "say" command.
+
---
+++ clearance_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+datafile = 'routes.dat'
+
+
+# J60:450:0:0.00:84:44.04:10483:ARTCC-BDRY::0::^@^@^@^@^@
+
+# read in routes data
+
+
+# generate random clearance
+
