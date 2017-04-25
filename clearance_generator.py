9e4f97fe8c7a386612f8c5a6902a5f66c9775950
Keith Resar <keith.resar@422long.com>
2017-04-24 20:45:28 -0500

    -

--- clearance_generator.py
+++ clearance_generator.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 
+import re
+import random
 
-datafile = 'routes.dat'
+datafile = 'holds.nav'
 
-
-# J60:450:0:0.00:84:44.04:10483:ARTCC-BDRY::0::^@^@^@^@^@
 
 # read in routes data
+routes = list()
+with open(datafile) as fh:
+    for l in fh.readlines():
+        routes.append(re.sub("\s+.*","",l.split(":")[0]))
 
 
+clearance = list()
+
+"""
+   “Cessna 12345 cleared to Nashville as filed, fly runway heading, climb and maintain three thousand, expect seven thousand one zero minutes after departure, departure frequency one two four point six five, squawk two seven one three.”
+"""
+
+# callsign
+clearance.append("Cessna seven three three tango papa")
+
+# route
+clearance.append("cleared to %s as filed" % random.choice(routes))
+
+# 
+
+#
 # generate random clearance
+#
+
+
+# say clearance
+
 
